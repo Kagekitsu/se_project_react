5d107f799c988a014f224c21ca6bf7d387426bde
Braden <baj147@yahoo.com>
2023-08-01 16:25:32 -0400

    updated project with adding component, image, utils, vendor and their subcat

---
+++ src/components/App/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  <div>
+    <header className='header'>
+      <div className='header__logo'>
+        <div>Logo</div>
+        <div>Date</div>
+      </div>
+      <div className='header__avatar-logo'>
+        <div>
+          <button type='text'>+ Add clothes</button>
+        </div>
+        <div>Terrence Tegegne</div>
+        <div>Avatar Logo</div>
+      </div>
+    </header>
+  </div>
+}
+
+export default App;
