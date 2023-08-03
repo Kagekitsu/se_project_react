79a2dd0b7c7272a1bd7cf71c6bfce1ec66aa9853
Braden <baj147@yahoo.com>
2023-08-02 20:12:57 -0400

    updated form button

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -13,13 +13,13 @@ const ModalWithForm = ({
         <button
           type="button"
           onClick={onClose}
-          className="modal__form_close_button"
-        ></button>
+          className="modal__form_close_button" />
         <h3 className="modal__title">{title}</h3>
-        <form className="modal__form">{children}</form>
+        <form className="modal__form">{children}
         <button type="submit" className="modal__submit_button">
           {buttonText}
         </button>
+        </form>
       </div>
     </div>
   );
